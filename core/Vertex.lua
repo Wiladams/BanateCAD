cb2c7509334b11e4a0312a9af203413aa1f0ed9f
William Adams <william_a_adams@msn.com>
2011-12-16 08:09:05 -0800

    Added Vertex.lua to normalize vertex representation, and support CSG

---
+++ core/Vertex.lua
@@ -0,0 +1,52 @@
+--require "Vector3D"
+
+Vertex = {}
+Vertex_mt = {__index = Vertex}
+
+
+function Vertex.new(pos, normal)
+	local this = {}
+	setmetatable(this, Vertex_mt)
+
+	this.pos = Vector3D.new(pos)
+	this.normal = Vector3D.new(normal)
+
+ 	return this
+end
+
+function Vertex.clone(this)
+    return Vertex.new(this.pos:clone(), this.normal:clone())
+end
+
+-- Invert all orientation-specific data (e.g. vertex normal). Called when the
+-- orientation of a polygon is flipped.
+function Vertex.flip(this)
+    this.normal = -this.normal -- :negated();
+
+	return this
+end
+
+-- Create a new vertex between this vertex and `other` by linearly
+-- interpolating all properties using a parameter of `t`. Subclasses should
+-- override this to interpolate additional properties.
+function Vertex.interpolate(self, other, t)
+    return Vertex.new(self.pos:lerp(other.pos, t), self.normal:lerp(other.normal, t))
+end
+
+function Vertex.tostring(v)
+	return '{'..v.pos:tostring()..','..v.normal:tostring()..'}'
+end
+
+-- Setup tostring
+Vertex_mt.__tostring = Vertex.tostring
+
+
+---[[
+
+	local v1 = Vertex.new({1,1,1},{0,0,1})
+
+	print(v1)
+
+	local v2 = v1:clone()
+	print("v1 - V2 clone: ", v1, v2)
+--]]
