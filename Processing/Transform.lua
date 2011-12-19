8b7d96467a95190256e060fea171c7fba2290bb3
William Adams <william_a_adams@msn.com>
2011-12-18 19:23:05 -0800

    Added some keyboard and mouse Added Rotate, Translate

---
+++ Processing/Transform.lua
@@ -0,0 +1,40 @@
+function applyMatrix()
+end
+
+function popMatrix()
+end
+
+function printMatrix()
+end
+
+function pushMatrix()
+end
+
+function resetMatrix()
+end
+
+function rotate(rad)
+	Processing.Rotate(0,0,rad)
+end
+
+function rotateX()
+end
+
+function rotateY()
+end
+
+function rotateZ()
+end
+
+function scale()
+end
+
+function shearX()
+end
+
+function shearY()
+end
+
+function translate(x, y, z)
+	Processing.Translate(x, y, z)
+end
