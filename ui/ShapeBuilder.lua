589075278fefe585a86aeda8836ae24b1281efed
William Adams <william_a_adams@msn.com>
2012-01-21 22:37:08 -0800

    fixed bezier in ShapeBuilder

--- ui/ShapeBuilder.lua
+++ ui/ShapeBuilder.lua
@@ -10,20 +10,22 @@ function ShapeBuilder.AddVertex(self, vtx)
 	table.insert(self.Vertices, Point3D(vtx))
 end
 
-function ShapeBuilder.Bezier(self, pt1, pt2, pt3)
+function ShapeBuilder.Bezier(self, p1, p2, p3)
 	-- If there isn't already a starting point,
 	-- then add one
 	local p0 = {0,0,0}
 	if #self.Vertices < 1 then
-		self:AddVertex(pt0)
+		self:AddVertex(p0)
 	else
 		p0 = self.Vertices[#self.Vertices]
 	end
 
 
 	local pts = {p0, p1, p2, p3}
+
 	local curveSteps = 30;
 
+---[[
 	local cv4 = cubic_vec3_to_cubic_vec4(pts);
 
 	for i=0, curveSteps do
@@ -31,6 +33,7 @@ function ShapeBuilder.Bezier(self, pt1, pt2, pt3)
 		local cpt = bezier_eval(u, cv4);
 		self:AddVertex(cpt)
 	end
+--]]
 end
 
 function ShapeBuilder.LineTo(self, pt1)
